a617f7435e7c9d4710e5152a295e3185292a805c
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2024-12-10 21:14:10 +0300

    tesrt

--- src/main/java/ru/itmo/App.java
+++ src/main/java/ru/itmo/App.java
@@ -1,10 +1,9 @@
 package ru.itmo;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Application;
+
+@Path("/api")
+public class App extends Application {
+    
 }
---
+++ src/main/java/ru/itmo/controller/PersonResources.java
@@ -0,0 +1,41 @@
+package ru.itmo.controller;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+import ru.itmo.model.Person;
+
+@Path("/persons")
+public class PersonResources {
+
+    @POST
+    @Consumes("application/xml")
+    public Response addPerson(Person person) {
+        return Response.status(Response.Status.CREATED).entity(person).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getPerson(@PathParam("id") Long id) {
+        Person person = new Person();
+        return Response.ok(person).build();
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @Consumes("application/xml")
+    public Response updatePerson(@PathParam("id") Long id, Person person) {
+        return Response.ok(person).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deletePerson(@PathParam("id") Long id) {
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+}
