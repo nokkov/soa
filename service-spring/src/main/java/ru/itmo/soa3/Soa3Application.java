5f81d1df6f601bc13d1058d814ce11ef10cdbb84
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2025-01-09 01:47:46 +0300

    add spring intilzr

---
+++ service-spring/src/main/java/ru/itmo/soa3/Soa3Application.java
@@ -0,0 +1,13 @@
+package ru.itmo.soa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soa3Application.class, args);
+	}
+
+}
---
+++ service-spring/src/test/java/ru/itmo/soa3/Soa3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.soa3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soa3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
